7bc4095d8b8b945a826e08cc80ce774a01f599b9
azka <azkaa5681@gmail.com>
2023-10-17 09:01:17 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "-------------------------------------" << endl;
+    cout << "Nama: Azka Reza Aditya" << endl << endl;
+    cout << "NPM: 2301631170127" << endl << endl;
+    cout << "Kelas: 1A - Informatika" << endl << endl;
+    cout << "Cita-cita: Progammer" << endl << endl;
+    cout << "Motivasi: Tertarik dengan pemrograman" << endl;
+    cout << "-------------------------------------" << endl << endl;
+    return 0;
+}
